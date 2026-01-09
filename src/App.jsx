5ebb24fb3a520449d0f2b59af63c00a3fa5381a7
Linus Godwin <linusgodwin2@gmail.com>
2026-01-09 20:48:25 +0100

    created a new brach to add the watch later functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
